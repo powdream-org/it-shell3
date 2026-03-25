e1fd43a2866e659cc989f2d444926c4404658d5e
Heejoon Kang <haseo.hackers@gmail.com>
2026-03-25 09:52:46 +0900

    feat(libitshell3): add Session and SessionEntry with slot management

--- modules/libitshell3/src/root.zig
+++ modules/libitshell3/src/root.zig
@@ -4,6 +4,7 @@ pub const types = @import("core/types.zig");
 pub const preedit_state = @import("core/preedit_state.zig");
 pub const split_tree = @import("core/split_tree.zig");
 pub const pane = @import("core/pane.zig");
+pub const session = @import("core/session.zig");
 
 test {
     std.testing.refAllDecls(@This());
